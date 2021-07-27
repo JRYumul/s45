3aacde621782a23e4a84fed3485f8bd866458d12
Joseph Yumul <joseph.yumul@tuitt.com>
2021-07-27 18:30:49 +0800

    Specific course

--- src/pages/SpecificCourse.js
+++ src/pages/SpecificCourse.js
@@ -1,4 +1,4 @@
-import { useEffect, useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { Container, Card, Button } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
 
@@ -6,23 +6,45 @@ import UserContext from '../UserContext';
 
 export default function SpecificCourse(){
 
+	const [name, setName] = useState("")
+	const [description, setDescription] = useState("")
+	const [price, setPrice] = useState(0)
+
 	const { user } = useContext(UserContext);
 	//useParams() contains any values we are trying to pass in the URL stored in a key/value pair
+
+	//useParams is how we receive the courseId passed via the URL
 	const { courseId } = useParams();
 
 	useEffect(()=> {
-		console.log(courseId)
+		// console.log(courseId)
+
+		//MINI ACTIVITY:
+		//Inside of this useEffect, fetch the data for the specific course and display the results in the console
+
+		//PART 2:
+		//Display the fetch request results in the component return statement
+
+		fetch(`${ process.env.REACT_APP_API_URL }/courses/${courseId}`)
+		.then(res => res.json())
+		.then(data => {
+			// console.log(data)
+			setName(data.name)
+			setDescription(data.description)
+			setPrice(data.price)
+		})
+
 	}, [])
 
 	return(
 		<Container>
 			<Card className="mt-5">
 				<Card.Header className="bg-dark text-white text-center pb-0">
-					<h4>Name</h4>
+					<h4>{name}</h4>
 				</Card.Header>
 				<Card.Body>
-					<Card.Text>Description</Card.Text>
-					<h6>Price: Php Price</h6>
+					<Card.Text>{description}</Card.Text>
+					<h6>Price: Php {price}</h6>
 				</Card.Body>
 				<Card.Footer>
 					{user.id !== null
